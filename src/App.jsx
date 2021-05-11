3435f898b961d63cce45b54212aab83f6dc7331e
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-11 15:52:34 +0200

    Generate Cards out of data array

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import * as React from 'react'
-import Pagination from './Pagination'
-import Button from './Button'
 import Card from './Card'
+import cardData from './questen-data.json'
 
 export default () => {
   return (
     <div>
-      <Button isActive>Active</Button>
-      <Button>not Active</Button>
-      <Pagination totalPages={34} currentPage={1} />
-      <Pagination totalPages={34} currentPage={2} />
-      <Pagination totalPages={34} currentPage={34} />
-      <Card text="ich bin ein kleiner Text" title="Titel" />
-      <Card
-        isBookmarkActive
-        text="ich bin anderer ein kleiner Text"
-        title="Titel 2"
-      />
+      {cardData.map(element => {
+        const { question, correct_answer } = element
+        return <Card title={question} text={correct_answer}></Card>
+      })}
     </div>
   )
 }
