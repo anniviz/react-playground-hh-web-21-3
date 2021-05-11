15a826ee7d923804227f5dbf9553ad8c84e1eaf7
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-11 16:26:01 +0200

    Add uuids to data array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import Card from './Card'
 import cardData from './questen-data.json'
+import { v4 as uuidv4 } from 'uuid'
 
 export default () => {
+  const cardDataWithIDs = cardData.map(data => ({
+    ...data,
+    id: uuidv4(),
+  }))
+  console.log(cardDataWithIDs)
   return (
     <div>
-      {cardData.map(element => {
-        const { question, correct_answer } = element
-        return <Card title={question} text={correct_answer}></Card>
+      {cardDataWithIDs.map(element => {
+        const { question, correct_answer, id } = element
+        return <Card key={id} title={question} text={correct_answer}></Card>
       })}
     </div>
   )
