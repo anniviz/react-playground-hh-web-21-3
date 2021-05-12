8073517b7889108a65c3fc417de7605505841d60
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-12 15:36:30 +0200

    Add slider to change the color of the box

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 import './App.css'
+
 export default () => {
   const [size, setSize] = useState(100)
+  const [colorHue, setColorHue] = useState(100)
   const style = {
     width: size + 'px',
     height: size + 'px',
+    background: 'hsl(' + colorHue + ', 100%, 50%)',
   }
   return (
     <div className="App">
       <label>
         Size:
-        <input value={size} onChange={handleChange} type="range" max="200" />
+        <input
+          value={size}
+          onChange={handleSizeChange}
+          type="range"
+          max="200"
+        />
+      </label>
+      <label>
+        Color:
+        <input
+          value={colorHue}
+          onChange={handleColorChange}
+          type="range"
+          max="360"
+        />
       </label>
       <div style={style} className="Box" />
     </div>
   )
-  function handleChange(event) {
+  function handleSizeChange(event) {
     const input = event.target
     setSize(input.value)
+  }
+
+  function handleColorChange(event) {
+    const input = event.target
+    setColorHue(input.value)
   }
 }
